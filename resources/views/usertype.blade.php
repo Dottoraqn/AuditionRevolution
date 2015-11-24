1684cd0ca9bb46f7ad264405d37d03591382f2ca
Emma Hargrave <emmah1024@gmail.com>
2015-11-24 11:54:39 -0500

    Added more secondary registration functionality -Emma

--- resources/views/usertype.blade.php
+++ resources/views/usertype.blade.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Register</div>
+                    <div class="panel-heading">Tell us about yourself</div>
                     <div class="panel-body">
                         @if (count($errors) > 0)
                             <div class="alert alert-danger">
@@ -24,26 +24,109 @@
                         @endif
                         <form class="form-horizontal" role="form" method="POST" action="/auth/register">
                             <input type="hidden" name="_token" value="{!! csrf_token() !!}">
-
+                        <!--DESC-->
                             <div class="form-group">
-                                <label class="col-md-4 control-label">Eye Color</label><br>
-                                <div class="col-md-6">
+                                <label class="col-sm-4 control-label">Description</label>
+                                <div class="col-sm-4"><textarea class="form-control" maxlength="200"></textarea></div>
+                            </div>
+                        <!--GEND-->
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label">Gender</label>
+                                <div class="col-sm-4">
                                     <div class="radio">
-                                        <label><input type="radio" name="optradio">Blue</label>
+                                        <label><input type="radio" name="optradio">Male</label><br>
+                                        <label><input type="radio" name="optradio">Female</label><br>
+                                        <label><input type="radio" name="optradio">Prefer not to specify</label><br>
                                     </div>
-                                    <div class="radio">
-                                        <label><input type="radio" name="optradio">Green</label>
-                                    </div>
-                                    <div class="radio">
-                                        <label><input type="radio" name="optradio">Brown</label>
-                                    </div>
-                                    <div class="radio">
-                                        <label><input type="radio" name="optradio">Hazel</label>
-                                    </div>
-                                   
-                                   
                                 </div>
                             </div>
+                        <!--ETHN-->
+                            <div class="form-group">
+                               <label class="col-sm-4 control-label">Ethnicity</label>
+                               <div class="col-sm-4">
+                                    <div class="radio">
+                                        <label><input type="radio" name="optradio">African American</label><br>
+                                        <label><input type="radio" name="optradio">Hispanic</label><br>
+                                        <label><input type="radio" name="optradio">East Asian</label><br>
+                                        <label><input type="radio" name="optradio">Indian</label><br>
+                                        <label><input type="radio" name="optradio">Middle Eastern</label>
+                                    </div>
+                               </div> 
+                            </div>
+                        <!--EYEC-->
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label bul">Eye Color</label>
+                                <div class="col-sm-4">
+                                    <div class="radio">
+                                        <label><input type="radio" name="optradio">Blue</label><br>
+                                        <label><input type="radio" name="optradio">Green</label><br>
+                                        <label><input type="radio" name="optradio">Brown</label><br>
+                                        <label><input type="radio" name="optradio">Hazel</label>
+                                    </div>
+                               </div> 
+                            </div>
+                        <!--HAIR-->
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label bul">Hair Color</label>
+                                <div class="col-sm-4">
+                                    <div class="radio">
+                                        <label><input type="radio" name="optradio">Brown</label><br>
+                                        <label><input type="radio" name="optradio">Blonde</label><br>
+                                        <label><input type="radio" name="optradio">Red</label><br>
+                                        <label><input type="radio" name="optradio">Black</label><br>
+                                        <label><input type="radio" name="optradio">Other</label>
+                                    </div>
+                               </div> 
+                            </div>
+                        <!--WEIG-->
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label bul">Weight</label>
+                                <div class="col-md-4">
+                                <input type="text" class="form-control" name="weight" value="{{ old('weight') }}">
+                                </div>
+                            </div>
+                        <!--HEIG-->
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label bul">Height</label>
+                                <div class="col-md-4">
+                                <input type="text" class="form-control" name="weight" value="{{ old('weight') }}">
+                                </div>
+                            </div>
+                        <!--VOCA-->
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label bul">Vocal Range</label>
+                                <div class="col-sm-4">
+                                    <div class="radio">
+                                        <label><input type="radio" name="optradio">Soprano</label><br>
+                                        <label><input type="radio" name="optradio">Mezzo-Soprano</label><br>
+                                        <label><input type="radio" name="optradio">Contralto</label><br>
+                                        <label><input type="radio" name="optradio">Countertenor</label><br>
+                                        <label><input type="radio" name="optradio">Tenor</label><br>
+                                        <label><input type="radio" name="optradio">Baritone</label><br>
+                                        <label><input type="radio" name="optradio">Bass</label>
+                                    </div>
+                               </div> 
+                            </div>
+                        <!--UNIO-->
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label bul">Union</label>
+                                <div class="col-sm-4">
+                                    <div class="checkbox">
+                                        <label><input type="checkbox" name="optcheckbox">None</label><br>
+                                        <label><input type="checkbox" name="optcheckbox">SAG-AFTRA</label><br>
+                                        <label><input type="checkbox" name="optcheckbox">AEA</label><br>
+                                        <label><input type="checkbox" name="optcheckbox">ACTRA</label><br>
+                                        <label><input type="checkbox" name="optcheckbox">AFM</label><br>
+                                        <label><input type="checkbox" name="optcheckbox">AGMA</label><br>
+                                        <label><input type="checkbox" name="optcheckbox">AGVA</label>
+                                    </div>
+                               </div> 
+                            </div>
+
+                           
+                            
+                            
+                            
 <!--
                             <div class="form-group">
                                 <label class="col-md-4 control-label">First Name</label>
