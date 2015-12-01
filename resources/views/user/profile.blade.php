5bbf715fd3bf10d894818a305794a55caa3e828d
Emma Hargrave <emmah1024@gmail.com>
2015-12-01 17:45:59 -0500

    updated userpage styles -emma

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -3,55 +3,69 @@
 @section('title', 'User Profile')
 
 @section('content')
-<?php echo $user; ?>
-<img class="headshot" src="{{$user->user_avatar->original_url}}" alt="headshot">
-<div class="personalInfo">
-    <div id="name">{{$user->first_name}} {{$user->last_name}}</div>
-    <div id="streetAddress">{{$user->address}}, <br>{{$user->city}}, {{$user->state}} {{$user->zipcode}}</div>
-    <div id="emailAddress">{{$user->email}}</div>
-    <div id="phoneNumber">{{$user->phone_number}}</div>
-
-    <div class="attributes">
-        <div class="attributeContent" id="eyeColor">
-            <img class="descriptor" src="../img/eye-template.png" alt="eye">
+<?php //echo $user; ?>
+<div class="container-fluid">
+<!--PERSONALINFO-->
+    <div class="row">
+       <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Personal Information</div>
+<!--                <img class="col-md-4 img-rounded" alt="Responsive image" src="{{$user->user_avatar->original_url}}" alt="headshot">-->
+                <div class="col-md-4">
+                    <h1>{{$user->first_name}} {{$user->last_name}}</h1>
+                    <h4>{{$user->email}}</h4>
+                    <h4>{{$user->phone_number}}</h4>
+                    <h4>{{$user->address}}, <br>{{$user->city}}, {{$user->state}} {{$user->zipcode}}</h4>
+                </div>
+                <br>
+                <div class="col-sm-4">
+                   <b>Eye Color:</b> {{$user->user_description->eye_color}}</br>
+                   <b>Hair Color:</b> {{$user->user_description->hair_color}}</br>
+                   <b>Ethnicity:</b> {{$user->user_description->ethnicity}}</br>
+                   <b>Weight:</b> {{$user->user_description->weight}}</br>
+                   <b>Vocal Range:</b> {{$user->user_description->vocal_range}}</br>
+                   <b>Union:</b> {{$user->user_description->union}}
+                </div>
+            </div>
         </div>
-        <!-- vocal range: two selectors, high->low -->
-
-        <div class="attributeContent" id="vocalRange"><p>C#-D#</p></div> 
-        <!-- needs to be changed for all clothing articles-->
-
-        <div class="attributeContent" id="dressSize"><p>02</p></div>
-        <!-- needs to be specified american or european-->
-
-        <div class="attributeContent" id="shoeSize"><p>05</p></div>
-        <!-- specified in ft or meters-->
-
-        <div class="attributeContent" id="height"><p>5'6</p></div>
-        <!-- lb/kg-->
-
-        <div class="attributeContent" id="weight"><p>130</p></div>
-
-        <div class="attributeContent" id="skinTone">
-            <img class="descriptor" src="img/skintone.png" alt="skin">
+    </div>
+<!--RESUME-->
+    <div class="row">
+       <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Resume</div>
+                @foreach($user->user_experience as $user_ex)
+                <div id="expwrap">
+                    <div class="row">
+                        <div class="col-sm-4"><h2>{{$user_ex->show_name}}<br><small>{{$user_ex->production_company}}</small></h2></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-2">{{$user_ex->role}}</div><div class="col-lg-4">{{$user_ex->description}}</div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+<!--SKILLS-->
+    <div class="row">
+       <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Skills</div>
+                <div class="experience">
+                  @foreach($user->user_experience as $user_ex)
+                   <div id="expwrap">
+                      <div class="row">
+                          <div class="col-sm-4"><h2>{{$user_ex->show_name}}<br><small>{{$user_ex->production_company}}</small></h2></div>
+                      </div>
+                      <div class="row">
+                          <div class="col-sm-2">{{$user_ex->role}}</div><div class="col-lg-4">{{$user_ex->description}}</div>
+                      </div>
+                   </div>
+                  @endforeach
+                </div>
+            </div>
         </div>
     </div>
 </div>
-
-<div class="workInfo">
-    <div class="profileSectionHeader">Experience</div>
-    <div class="experience">
-      @foreach($user->user_experience as $user_ex)
-        <div class="experienceWraper">
-            <div class="playName">{{$user_ex->show_name}}</div>
-            <div class="characterName">{{$user_ex->role}}</div>
-            <div class="characterName">{{$user_ex->description}}</div>
-            <div class="characterName">{{$user_ex->production_company}}</div>
-        </div>
-      @endforeach
-    </div>
-
-    <div class="profileSectionHeader">Skills</div>
-    <div class="skills">{{$user->user_resume->skills}}</div>
-</div>
-
 @endsection
